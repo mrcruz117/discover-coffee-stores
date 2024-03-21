7d82be1319c6a9f8f71fa5d5ac5e337527ffaaa9
Michael Cruz <mcruz@aersale.com>
2024-03-21 16:28:59 -0400

    styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14  bg-purple-951">
+    <main className="flex min-h-screen flex-col items-center justify-between p-14  backgroundImage">
       <h1>Coffee Connoisseur</h1>
+      <h2>The Best Coffee Shops</h2>
     </main>
   );
 }
