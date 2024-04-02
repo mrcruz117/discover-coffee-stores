85f5d0d6cda93a679b6e9a8757f342c6049d570d
Michael Cruz <mcruz@aersale.com>
2024-04-02 12:01:04 -0400

    card working with dummy data. title added

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,59 @@ import Link from "next/link";
 
 export default function Home() {
   const storeId = "dark-horse-coffee";
+
+  const dummyData = [
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+    {
+      name: "Dark Horse Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
+    },
+    {
+      name: "StrangeLove Coffee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
+    },
+  ];
+
   return (
     <div className="mb-56">
       <main className="mx-auto mt-10 max-w-6xl px-4">
         <Banner />
+        <div className="mt-20">
+          <h2 className="mt-8 pb-8 text-4xl font-bold text-white">
+            Toronto Stores
+          </h2>
+        </div>
+
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
-          <Card
-            name="Dark Horse Coffee"
-            imgUrl="/static/hero-image.png"
-            href={`/coffee-store/${storeId}`}
-          />
+          {dummyData.map((store, idx) => (
+            <Card
+              key={`${store.name}-${idx}`}
+              name={store.name}
+              imgUrl={store.imgUrl}
+              href={`/coffee-store/${idx}`}
+            />
+          ))}
         </div>
       </main>
     </div>
