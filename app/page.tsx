e85769794b9e3a25e93a8ec95da681f161c0276f
Michael Cruz <mcruz@aersale.com>
2024-04-09 11:48:20 -0400

    created types. showing fetched coffee data

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/banner.client";
 import Card from "@/components/card.server";
 import { fetchCoffeeStores } from "@/lib/coffee-stores";
+import { CoffeeStoreType } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,42 +11,7 @@ async function getStores() {
 
 export default async function Home() {
   const storeId = "dark-horse-coffee";
-  const { features } = await getStores();
-
-  // console.log("stores", stores);
-
-  const dummyData = [
-    {
-      name: "Dark Horse Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
-    },
-    {
-      name: "StrangeLove Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
-    },
-    {
-      name: "Dark Horse Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
-    },
-    {
-      name: "StrangeLove Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
-    },
-    {
-      name: "Dark Horse Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80",
-    },
-    {
-      name: "StrangeLove Coffee",
-      imgUrl:
-        "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixid=MnwxMjA3fDB8HxwaG90by1w/WdLfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80",
-    },
-  ];
+  const coffeeStores = await getStores();
 
   return (
     <div className="mb-56">
@@ -58,7 +24,7 @@ export default async function Home() {
         </div>
 
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-2 lg:grid-cols-3 lg:gap-6">
-          {dummyData.map((store, idx) => (
+          {coffeeStores.map((store: CoffeeStoreType, idx: number) => (
             <Card
               key={`${store.name}-${idx}`}
               name={store.name}
