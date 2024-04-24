5e17fcf9e944aa9ce372bad3f577fe96eb1ec24b
Michael Cruz <mcruz@aersale.com>
2024-04-24 16:22:24 -0400

    added geolocation hook and button

---
+++ components/nearby-coffee-stores.client.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React, { useEffect } from "react";
+import Banner from "./banner.client";
+import useTrackLocation from "@/hooks/use-track-location";
+
+export default function NearbyCoffeeStores() {
+  const { longLat, isFindingLocation, handleTrackLocation, locationErrorMsg } =
+    useTrackLocation();
+
+  const handleOnClick = () => {
+    handleTrackLocation();
+  };
+  return (
+    <div>
+      <Banner
+        handleOnClick={handleOnClick}
+        buttonText={isFindingLocation ? "Loading..." : "View Stores Nearby"}
+      />
+    </div>
+  );
+}
