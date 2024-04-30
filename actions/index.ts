7088e629a5e49dbc79a235129d682299cc622adf
Michael Cruz <mcruz@aersale.com>
2024-04-30 17:47:48 -0400

    using useFormState and status updating and rerendeering voting value

--- actions/index.ts
+++ actions/index.ts
@@ -2,9 +2,22 @@
 
 import { updateCoffeeStore } from "@/lib/airtable";
 
-export const upvoteAction = async (id: string) => {
-  console.log("upvote action", id);
+type State = {
+  id: string;
+};
+
+export const upvoteAction = async (prevState: State) => {
+  console.log("upvote action");
+
+  const { id } = prevState;
 
   const data = await updateCoffeeStore(id);
-  console.log("data: ", data);
+  console.log({ data });
+
+  if (data) {
+    return {
+      voting: data.length > 0 ? data[0].voting : 0,
+      id,
+    };
+  }
 };
--- app/coffee-store/[id]/page.tsx
+++ app/coffee-store/[id]/page.tsx
@@ -1,16 +1,14 @@
-import Upvote from "@/components/upvote.client";
-import { createCoffeeStore } from "@/lib/airtable";
-import { fetchCoffeeStore, fetchCoffeeStores } from "@/lib/coffee-stores";
-import { CoffeeStoreType } from "@/types";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+import { fetchCoffeeStore, fetchCoffeeStores } from "@/lib/coffee-stores";
+import Image from "next/image";
+import { CoffeeStoreType } from "@/types";
+import { createCoffeeStore } from "@/lib/airtable";
+import Upvote from "@/components/upvote.client";
 
 async function getData(id: string, queryId: string) {
   const coffeeStoreFromMapbox = await fetchCoffeeStore(id, queryId);
   const _createCoffeeStore = await createCoffeeStore(coffeeStoreFromMapbox, id);
-
-  console.log("createCoffeeStore: ", createCoffeeStore);
 
   const voting = _createCoffeeStore ? _createCoffeeStore[0].voting : 0;
 
@@ -21,9 +19,10 @@ async function getData(id: string, queryId: string) {
       }
     : {};
 }
+
 export async function generateStaticParams() {
-  const TOKYO_LONG_LAT = "139.77377541514397%2C35.67164056369268";
-  const coffeeStores = await fetchCoffeeStores(TOKYO_LONG_LAT, 6);
+  const TORONTO_LONG_LAT = "-79.3789680885594%2C43.653833032607096";
+  const coffeeStores = await fetchCoffeeStores(TORONTO_LONG_LAT, 6);
 
   return coffeeStores.map((coffeeStore: CoffeeStoreType) => ({
     id: coffeeStore.id.toString(),
@@ -41,8 +40,8 @@ export default async function Page(props: {
 
   const coffeeStore = await getData(id, queryId);
 
-  const { name = "", address = "", imgUrl = "", voting = 0 } = coffeeStore;
-  console.log("store: ", coffeeStore);
+  const { name = "", address = "", imgUrl = "", voting } = coffeeStore;
+
   return (
     <div className="h-full pb-80">
       <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
@@ -54,7 +53,10 @@ export default async function Page(props: {
             <h1 className="text-4xl">{name}</h1>
           </div>
           <Image
-            src={imgUrl}
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
             width={740}
             height={360}
             className="max-h-[420px] min-w-full max-w-full rounded-lg border-2 sepia lg:max-w-[470px] "
@@ -66,7 +68,7 @@ export default async function Page(props: {
           {address && (
             <div className="mb-4 flex">
               <Image
-                src="/static/places.svg"
+                src="/static/icons/places.svg"
                 width="24"
                 height="24"
                 alt="places icon"
@@ -74,7 +76,7 @@ export default async function Page(props: {
               <p className="pl-2">{address}</p>
             </div>
           )}
-          <Upvote id={id} voting={voting} />
+          <Upvote voting={voting} id={id} />
         </div>
       </div>
     </div>
